f7fd73925385e0bebef7454539947889613b0aeb
danujawijerathne <danujawijerathne45@gmail.com>
2024-12-02 22:59:01 +0530

    login/register UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.gms.location)
     implementation(libs.tensorflow)
+    implementation(libs.lottie)
 }
